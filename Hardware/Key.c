2a6866e22df6ccf64356550148bf65fd53d6f5aa
colorcard <liwq6688@icloud.com>
2024-11-26 21:51:27 +0800

    Menu Add.

--- Hardware/Key.c
+++ Hardware/Key.c
@@ -11,6 +11,7 @@ void Key_Init(void)
 	/*开启时钟*/
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);		//开启GPIOB的时钟
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
 	
 	/*GPIO初始化*/
 	GPIO_InitTypeDef GPIO_InitStructure;
@@ -21,6 +22,9 @@ void Key_Init(void)
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
 }
 
 /**
@@ -48,6 +52,14 @@ uint8_t Key_GetNum(void)
 		Delay_ms(20);											//延时消抖
 		KeyNum = 2;												//置键码为2
 	}
+
+    if (GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_2) == 1)			//Key3 Set
+    {
+        Delay_ms(20);											//延时消抖
+        while (GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_2) == 1);	//等待按键松手
+        Delay_ms(20);											//延时消抖
+        KeyNum = 3;												//置键码为2
+    }
 	
 	return KeyNum;			//返回键码值，如果没有按键按下，所有if都不成立，则键码为默认值0
 }
--- User/main.c
+++ User/main.c
@@ -4,42 +4,129 @@
 #include "Motor.h"
 #include "Key.h"
 
+#define ADDKey 2
+#define DECKey 1
+#define SETKey 3
+
 uint8_t KeyNum;		//定义用于接收按键键码的变量
+uint8_t KeyNumMenu=1;		//定义用于接收按键页码
+uint8_t KeyNumMenuTotal=1;		//定义用于接收按键页码的总和
+uint8_t MenuFlag=0;        //定义菜单标志,0为主菜单,1为子菜单
+
+
 int8_t Speed;		//定义速度变量
+
+
 
 int main(void) {
     /*模块初始化*/
-//	OLED_Init();
-//	OLED_ShowString(1,1,"HelloWorld!");
+    OLED_Init();
 	GPIO_PinRemapConfig(GPIO_Remap_SWJ_Disable, ENABLE); // 禁用 JTAG/SWD 调试引脚
 	Motor_Init();
 	Key_Init();
 
+    /*Menu*/
+    OLED_ShowString(1,1,"1 Speed");//1 Speed
+
+
+
+
+
 
     while (1) {
-		KeyNum = Key_GetNum();				//获取按键键码
-		if (KeyNum == 2)					//按键1按下
-		{
-			Speed += 20;					//速度变量自增20
-			if (Speed > 100)				//速度变量超过100后
-			{
-				Speed = -100;				//速度变量变为-100
-											//此操作会让电机旋转方向突然改变，可能会因供电不足而导致单片机复位
-											//若出现了此现象，则应避免使用这样的操作
-			}
-		}
-		if (KeyNum == 1)					//按键1按下
-		{
-			Speed -= 20;					//速度变量自增20
-			if (Speed < -100)				//速度变量超过100后
-			{
-				Speed = 100;				//速度变量变为-100
-											//此操作会让电机旋转方向突然改变，可能会因供电不足而导致单片机复位
-											//若出现了此现象，则应避免使用这样的操作
-			}
-		}
-		Motor_SetSpeedA(Speed);				//设置直流电机的速度为速度变量
-		Motor_SetSpeedB(Speed);
-        
+
+        KeyNum = Key_GetNum();				//获取按键键码
+
+        if (KeyNum==ADDKey && KeyNumMenu<KeyNumMenuTotal && MenuFlag==0)   KeyNumMenu++;
+        if (KeyNum==DECKey && KeyNumMenu>1 && MenuFlag==0)   KeyNumMenu--;
+        if (KeyNum==SETKey)  MenuFlag=~MenuFlag;
+
+
+        /*显示菜单*/
+        if (MenuFlag==0)
+        {
+            if (KeyNumMenu==1)
+            {
+                OLED_Clear();
+                OLED_ShowString(1,1,"X Speed");//1 Speed
+                OLED_ShowString(1,3,"2 Test");//2 Menu2
+//                OLED_ShowString(1,5,"3 Menu3");//3 Menu3
+//                OLED_ShowString(1,7,"4 Menu4");//4 Menu4
+            }
+            if (KeyNumMenu==2)
+            {
+                OLED_Clear();
+                OLED_ShowString(1,1,"1 Speed");//1 Speed
+                OLED_ShowString(1,3,"X Test");//2 Menu2
+//                OLED_ShowString(1,5,"3 Menu3");//3 Menu3
+//                OLED_ShowString(1,7,"4 Menu4");//4 Menu4
+            }
+        }
+
+        /*菜单操作*/
+        if (MenuFlag==SETKey){
+
+
+            if (KeyNumMenu==1)//速度操作
+            {
+                OLED_Clear();
+                OLED_ShowString(1,1,"Speed");//1 Speed
+                OLED_ShowNum(1,1,Speed,4);//SpeedNum
+
+                if (KeyNum == ADDKey)					//按键1按下
+                {
+                    Speed += 20;					//速度变量自增20
+                    if (Speed > 100)				//速度变量超过100后
+                    {
+                        Speed = -100;				//速度变量变为-100
+                        //此操作会让电机旋转方向突然改变，可能会因供电不足而导致单片机复位
+                        //若出现了此现象，则应避免使用这样的操作
+                    }
+                }
+                if (KeyNum == DECKey)					//按键1按下
+                {
+                    Speed -= 20;					//速度变量自增20
+                    if (Speed < -100)				//速度变量超过100后
+                    {
+                        Speed = 100;				//速度变量变为-100
+                        //此操作会让电机旋转方向突然改变，可能会因供电不足而导致单片机复位
+                        //若出现了此现象，则应避免使用这样的操作
+                    }
+                }
+
+
+                Motor_SetSpeedA(Speed);				//设置直流电机的速度为速度变量
+                Motor_SetSpeedB(Speed);
+            }
+
+
+
+
+            if (KeyNumMenu==2)//目前测试的进程
+            {
+                OLED_Clear();
+                OLED_ShowString(1,1,"Test");//1 Speed
+            }
+
+
+
+
+
+
+
 	}
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
