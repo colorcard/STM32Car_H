a3487ae8b8379dc8539afceadd08bd2b4b80e3fb
colorcard <liwq6688@icloud.com>
2025-01-13 18:21:16 +0800

    排版

--- User/main.c
+++ User/main.c
@@ -9,15 +9,19 @@
 #include "Serial.h"
 #include "gray_track.h"
 
-#define ADDKey 2
-#define DECKey 1
-#define SETKey 3
+
+
+
+
+
 
 #define LEFT 1
 #define RIGHT 2
 
-
-
+/*---------------------按键定义---------------------*/
+#define ADDKey 2
+#define DECKey 1
+#define SETKey 3
 
 uint8_t KeyNum;		//定义用于接收按键键码的变量
 uint8_t KeyNumMenu=1;		//定义用于接收按键页码
@@ -27,20 +31,20 @@ uint8_t MenuFlag=0;        //定义菜单标志,0为主菜单,1为子菜单
 uint8_t LastKeyNumMenu=1;		//定义用于上一次接收按键页码
 uint8_t LastMenuFlag=0;        //上一次定义菜单标志,0为主菜单,1为子菜单
 
-int Temp=0;
 
 
 
+/*---------------------速度定义---------------------*/
 int16_t Speed;		//定义速度变量
 
 Encoder ecd_left;
 Encoder ecd_right;
 
 PID vec_left;
-PID pos_left;
 PID vec_right;
-PID pos_right;
 
+
+/*---------------------初始化函数---------------------*/
 void myCarControlCodeInit(){
     Parameter param = {4, 28, 13, 0.065};
     initEncoder(&ecd_left,param);
@@ -53,30 +57,15 @@ void myCarControlCodeInit(){
     initPID(&vec_right, 2100, 5000);
     setPIDParam(&vec_right, 11, 0.5,0.5);
     setPIDTarget(&vec_right, 0);
-
-    //pos pid init
-    //initPID(&pos_left, 300, 5000);// 300 -> rpm
-    //setPIDParam(&pos_left, 0.9, 0.0,0.0);
-    //setPIDTarget(&pos_left, 0);
-
-    //initPID(&pos_right, 300, 5000);
-    //setPIDParam(&pos_right, 0.9, 0.0,0.0);
-    //setPIDTarget(&pos_right, 0);
-
 }
 
 
 
-
+/*---------------------主函数---------------------*/
 
 int main(void) {
     /*模块初始化*/
     OLED_Init();
-	//GPIO_PinRemapConfig(GPIO_Remap_SWJ_Disable, ENABLE); // 禁用 JTAG/SWD 调试引脚
-	
-	
-	
-	
 	Motor_Init();
     Encoder_Init_TIM_All();
     Timer_Init();//定时器初始化
@@ -93,19 +82,11 @@ int main(void) {
 	//Delay_ms(1000);
 
 
-
-
-
-    /*Menu*/
-    OLED_ShowString(1,1,"1 Speed");//1 Speed
-
-
-
-
-
-
-
     while (1) {
+
+        Motor_SetSpeedA(vec_left.output);
+        Motor_SetSpeedB(vec_right.output);
+
         if (LastKeyNumMenu!=KeyNumMenu||LastMenuFlag!=MenuFlag){
             OLED_Clear();
             LastKeyNumMenu=KeyNumMenu;
@@ -146,48 +127,16 @@ int main(void) {
             /*  速度操作 */
             if (KeyNumMenu==1)
             {
-				//motor_target_set(100,100);
-				//setPIDTarget(&vec_right, 100);
-				//setPIDTarget(&vec_left, 100);
-				
-				
-				//Motor_SetSpeedB(200);
-				//Motor_SetSpeedA(200);
-				
 				
 				
                 OLED_ShowString(1,1,"Speed");//1 Speed
                 OLED_ShowSignedNum(2,1,Speed,5);//SpeedNum
                 OLED_ShowSignedNum(3,1,(int32_t)ecd_left.counter.count_increment,8);//
                 OLED_ShowSignedNum(4,1,(int32_t)ecd_right.counter.count_increment,8);//
-//                OLED_ShowNum(4,1,Temp,8);//
 
                 Serial_Printf("Speed:%lld,",ecd_left.counter.count_increment);
 				Serial_Printf("%lld\n",ecd_right.counter.count_increment);
-//                if (KeyNum == ADDKey)
-//                {
-//                    Speed += 200;					//速度变量自增20
-//                    if (Speed > 4000)				//速度变量超过100后
-//                    {
-//                        Speed = 4000;				//速度变量变为-100
-//                        //此操作会让电机旋转方向突然改变，可能会因供电不足而导致单片机复位
-//                        //若出现了此现象，则应避免使用这样的操作
-//                    }
-//                }
-//                if (KeyNum == DECKey)
-//                {
-//                    Speed -= 200;					//速度变量自增20
-//                    if (Speed < -4000)				//速度变量超过100后
-//                    {
-//                        Speed = -4000;				//速度变量变为-100
-//                        //此操作会让电机旋转方向突然改变，可能会因供电不足而导致单片机复位
-//                        //若出现了此现象，则应避免使用这样的操作
-//                    }
-//                }
 
-
-//                Motor_SetSpeedA(Speed);				//设置直流电机的速度为速度变量
-//                Motor_SetSpeedB(Speed);
             }
 
 
@@ -235,31 +184,16 @@ int main(void) {
 }//main函数的大括号
 
 
-
-void TIM1_UP_IRQHandler(void)
+/*---------------------定时器中断函数---------------------*/
+void TIM1_UP_IRQHandler(void)//100ms
 {
     if (TIM_GetITStatus(TIM1, TIM_IT_Update) == SET)
     {
-        Temp = TIM_GetCounter(TIM2);
         updateEncoderLoopSimpleVersion(&ecd_left, 100, TIM2);
         updateEncoderLoopSimpleVersion(&ecd_right, 100, TIM4);
 
-//        updatePID(&pos_left, ecd_left.position.angle);
-//        setPIDTarget(&vec_left, pos_left.output);
-//        updatePID(&vec_left, ecd_left.velocity.angular);
-//
-//        updatePID(&pos_right, ecd_right.position.angle);
-//        setPIDTarget(&vec_right, pos_right.output);
-//        updatePID(&vec_right, ecd_right.velocity.angular);
-
         updatePID(&vec_left, ecd_left.counter.count_increment);
         updatePID(&vec_right, ecd_right.counter.count_increment);
-
-
-        Motor_SetSpeedA(vec_left.output);
-        Motor_SetSpeedB(vec_right.output);
-		
-
 
 
         TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
